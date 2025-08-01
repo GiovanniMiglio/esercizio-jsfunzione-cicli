cb09d2521e554e5920e93388e3c9a7de35d87fb6
GiovanniMiglio <gmiglio89@gmail.com>
2025-08-01 12:17:39 +0200

    svolto esercizio 2

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ console.log( "eccellente")
 
 
 // esercizio 1 switc
+/*
 let v = 30
 let risultato
 
@@ -50,10 +51,68 @@ switch(true){
     risultato =  "ottimo";
     console.log(risultato);
     break;
-    
+
     case (v == 30):
     risultato =  "eccellente";
     console.log(risultato);
     break;
     
+}*/
+
+//esercizio 2 if/else if/else
+
+
+   /* let temperatura = prompt("Quanti gradi ci sono oggi?")
+    if (temperatura < 20 && temperatura > 0) {
+        console.log("non ci sono piu’ le mezze stagioni");
+        
+        
+    }else if(temperatura >= 30 ){
+        console.log("lu mare, lu sole, lu ientu");
+        
+        
+    }else if(temperatura < 30 && temperatura >= 20 ){
+        console.log("mi dia una peroni sudata");
+        
+        
+    }else if(temperatura < 0 && temperatura > -10 ){
+        console.log("non e' tanto il freddo quanto l'umidità");
+        
+        
+    }else if(temperatura <= -10 ){
+        console.log("copriti…ancora ti raffreddi");
+        
+        
+    }*/
+   // esercizio 2 switch 
+let temperatura = prompt("Quanti gradi ci sono oggi?")
+let risp
+switch (true) {
+    case ( temperatura < 20 && temperatura > 0):
+        risp = "non ci sono piu’ le mezze stagioni";
+        console.log(risp);
+        
+        break;
+    case ( temperatura >= 30):
+        risp = "lu mare, lu sole, lu ientu";
+        console.log(risp);
+        
+        break;
+   case ( temperatura < 30 && temperatura >= 20):
+        risp = "mi dia una peroni sudata";
+        console.log(risp);
+        
+        break;
+       case ( temperatura < 0 && temperatura > -10):
+        risp = "non e' tanto il freddo quanto l'umidità";
+        console.log(risp);
+        
+        break;
+    case ( temperatura <= -10):
+        risp = "copriti…ancora ti raffreddi";
+        console.log(risp);
+
+        break;
+        
 }
+    
